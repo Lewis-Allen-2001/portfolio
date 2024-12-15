ceab637ae832c1a71b72d3900f02bc5099be2b74
git <itslewbie01@gmail.com>
2024-12-15 12:55:38 +0000

    cleaned a few things up

--- src/app/feedback/page.jsx
+++ src/app/feedback/page.jsx
@@ -26,7 +26,7 @@ export default async function Feedback() {
   return (
     <>
       <div className="flex flex-col items-center py-10 px-4 bg-gray-900 text-white min-h-screen space-y-8">
-        {/* Page Header */}
+       
         <h1 className="text-3xl font-bold text-blue-400">
           Leave Some Feedback!
         </h1>
@@ -35,7 +35,6 @@ export default async function Feedback() {
           better at what I love to do!
         </h3>
 
-        {/* Feedback Form */}
         <form
           action={handleCreateFeedback}
           className="flex flex-col space-y-4 w-full max-w-lg"
@@ -48,7 +47,7 @@ export default async function Feedback() {
 
           <textarea
             name="topic"
-            placeholder="Topic of feedback"
+            placeholder="Topic"
             className="p-3 rounded-md border-2 border-gray-700 bg-gray-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400"
           />
 
@@ -66,7 +65,6 @@ export default async function Feedback() {
           </button>
         </form>
 
-        {/* Feedback Section */}
         <h3 className="text-2xl font-semibold text-blue-400">Feedback</h3>
         <div className="flex flex-col space-y-4 w-full max-w-2xl">
           {feedback.rows.map((fb, index) => {
@@ -76,10 +74,10 @@ export default async function Feedback() {
                 className="p-4 rounded-md bg-gray-800 border border-gray-700 shadow-md"
               >
                 <h3 className="text-lg font-bold text-blue-400">
-                  {fb.username}
+                  {fb.username} Said:
                 </h3>
-                <h4 className="text-gray-300">{fb.topic}</h4>
-                <p className="text-gray-400">{fb.feedback}</p>
+                <h4 className="text-gray-400">Topic: {fb.topic}</h4>
+                <p className="text-gray-300">{fb.feedback}</p>
               </div>
             );
           })}
